f5cb014e4edc44bb5f08e3ad1d5e355b81abc57e
sukhvirdi7 <sukh.virdi2@mhra.gov.uk>
2020-10-13 10:31:19 +0100

    characterCount and wordCount

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-var input = document.querySelectorAll('textarea')[0]
+var input = document.querySelectorAll('textarea')[0],
+  characterCount = document.querySelector('#characterCount'),
+  wordCount = document.querySelector('#wordCount');
+  
 input.addEventListener('keyup', function() {
+  
+  console.clear();
+  
+  characterCount.innerHTML = input.value.length;
+  
+  var words = input.value.match(/\b[-?(\w+)?]+\b/gi);
+
+  if (words) {
+    wordCount.innerHTML = words.length;
+  } else {
+    wordCount.innerHTML = 0;
+  }
 
 });
-
-
