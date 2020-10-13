dcecc588b645c4c4a0e57c3924310b468bd63239
sukhvirdi7 <sukh.virdi2@mhra.gov.uk>
2020-10-13 12:28:13 +0100

    sentenceCount and averageWordLength

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-var input = document.querySelectorAll('textarea')[0],
-  characterCount = document.querySelector('#characterCount'),
-  wordCount = document.querySelector('#wordCount');
+var input = document.querySelectorAll('textarea')[0]
+  characterCount = document.querySelector('#characterCount')
+  wordCount = document.querySelector('#wordCount')
+  sentenceCount = document.querySelector('#sentenceCount')  
+  averageWords = document.querySelector('#averageWords')
   
 input.addEventListener('keyup', function() {
   
   console.clear();
   
-  characterCount.innerHTML = input.value.length;
+
+  var characters = input.value.length //- input.value.split(" ").length + 1;
+  characterCount.innerHTML = characters
   
   var words = input.value.match(/\b[-?(\w+)?]+\b/gi);
 
@@ -14,6 +18,18 @@ input.addEventListener('keyup', function() {
     wordCount.innerHTML = words.length;
   } else {
     wordCount.innerHTML = 0;
+  }
+
+  if (words) {
+    var sentences = input.value.split(/[.|!|?]+/g);
+    var averageWordCharacters = words.toString().length - (words.length - 1);
+    var averageWordsjs = (averageWordCharacters / words.length)
+    console.log(words.toString())
+    sentenceCount.innerHTML = sentences.length - 1;
+    averageWords.innerHTML = averageWordsjs
+  } else {
+    sentenceCount.innerHTML = 0;
+    averageWords.innerHTML = 0;
   }
 
 });
